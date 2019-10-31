e424d7556767e6a7fbb229b545ea2a28746f03ba
cmaheo <c.maheo@lectra.com>
2019-10-31 10:03:08 +0100

    first commit

---
+++ BourseApp/app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // plus necessaire car mis directement dans le fichier route/web.php
+        //$this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
